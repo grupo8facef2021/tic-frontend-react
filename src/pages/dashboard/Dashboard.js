85ed51b0307dc69a86f324be4c090fa4840e2c4e
thiago prado <thiagoprado549@gmail.com>
2021-11-21 13:41:02 -0300

    iniciando tela de dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,68 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { useAlert } from 'react-alert';
+import { Col, Container, Row } from 'react-bootstrap';
+import { CardList, Text } from '../../components';
+import { CardContent, Content, Header } from '../../components/layout/Layout';
+
+import { getActivities } from '../../services/activities/activitiesService';
 
 const Dashboard = () => {
+  const alert = useAlert();
+  const [activities, setActivities] = useState([]);
+
+  useEffect(async () => {
+    const response = await getActivities();
+
+    if (!response.success) {
+      alert.error(response.message);
+    } else {
+      setActivities(response.data);
+    }
+  }, [activities]);
+
   return (
     <Container fluid>
-      <h3>Dashboard</h3>
+      <Content>
+        <Header />
+        <Row>
+          <Col>
+            <CardContent>
+              <Row>
+                <Col>
+                  <Text large text="Atividades" />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <CardList
+                    dataList={activities}
+                    templateCard={[
+                      {
+                        key: 'Nome',
+                        accessor: 'name',
+                      },
+                      {
+                        key: 'Cargo',
+                        accessor: 'role',
+                      },
+                    ]}
+                  />
+                </Col>
+              </Row>
+            </CardContent>
+          </Col>
+          <Col>
+            <CardContent>
+              <Text large text="Fazendo" />
+            </CardContent>
+          </Col>
+          <Col>
+            <CardContent>
+              <Text large text="Concluído" />
+            </CardContent>
+          </Col>
+        </Row>
+      </Content>
     </Container>
   );
 };
